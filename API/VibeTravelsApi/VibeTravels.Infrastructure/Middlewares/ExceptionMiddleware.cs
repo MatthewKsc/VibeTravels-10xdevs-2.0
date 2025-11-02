db5740ba0e71b22cd53cf030e67b41e11b82fa6b
MatthewKsc <55882615+MatthewKsc@users.noreply.github.com>
2025-11-02 17:56:59 +0100

    Add exception middleware

---
+++ API/VibeTravelsApi/VibeTravels.Infrastructure/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Http;
+using VibeTravels.Infrastructure.Models;
+using VibeTravels.Shared.Exceptions;
+
+namespace VibeTravels.Infrastructure.Middlewares;
+
+internal sealed class ExceptionMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception exception)
+        {
+            await HandleExceptionAsync(exception, context);
+        }
+    }
+    
+    private async Task HandleExceptionAsync(Exception exception, HttpContext context)
+    {
+        (int statusCode, Error error) = exception switch
+        {
+            VibeTravelsException => (StatusCodes.Status400BadRequest,
+                new Error(exception.GetType().Name.Underscore().Replace("_exception", string.Empty), exception.Message)),
+            _ => (StatusCodes.Status500InternalServerError, new Error("unexpected_error", "An unexpected error occurred."))
+        };
+
+        context.Response.StatusCode = statusCode;
+        await context.Response.WriteAsJsonAsync(error);
+    }
+}
