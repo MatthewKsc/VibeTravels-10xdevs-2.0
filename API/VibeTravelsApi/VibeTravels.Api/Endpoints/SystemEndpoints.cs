1c2146dd9b3cf604f848fb5663ab59f09ff1d47d
MatthewKsc <55882615+MatthewKsc@users.noreply.github.com>
2025-11-14 20:50:22 +0100

    Adjust endpoints

---
+++ API/VibeTravelsApi/VibeTravels.Api/Endpoints/SystemEndpoints.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace VibeTravels.Api.Endpoints;
+
+public static class SystemEndpoints
+{
+    public static void MapSystemEndpoints(this RouteGroupBuilder api)
+    {
+        api.MapGet("/health", [AllowAnonymous] () => Results.Ok("Api Alive"))
+            .WithName("HealthCheck")
+            .AllowAnonymous();
+    }
+}
+
